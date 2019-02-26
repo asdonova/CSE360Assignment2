a58f5998de00eb2f587180be7f8998c893726d17
drhs2 <drhs2@LAPTOP-ICITVKF8.3520.dhcp.asu.edu>
2019-02-25 18:47:48 -0700

    Final changes added to project. Added a private string named history that keeps track of the arithmetic done. GetHistory is completed.

--- CSE360Homework2/src/cse360assign2/Calculator.java
+++ CSE360Homework2/src/cse360assign2/Calculator.java
@@ -13,14 +13,19 @@
 package cse360assign2;
 
 /**
- * This class contains 1 private instance variable which is total. There are 5 functions contained
- * within this class:
+ * This class contains 2 private instance variables which is total
+ * and history. Total is used to display the output of all
+ * the functions performed on itself. History will keep track
+ * of all arithmetic operations performed.
+ * There are 5 functions contained within this class:
  * getTotal will return the total value.
  * Add will add the total with the parameter.
  * Subtract will subtract the total from the parameter.
  * Multiply will multiply the total from the parameter.
  * Divide will divide total by the parameter.
  * getHistory will return all the actions performed thus far.
+ * All of the arithmetic functions will add to the its arithmetic to the history
+ * string.
  * The constructor Calculator is called when creating an object of this class which
  * sets total to 0.
  * 
@@ -29,7 +34,7 @@ package cse360assign2;
 public class Calculator {
 
 	private int total;
-	
+	private String history ="0";
 	/**
 	 * This is the constructor of the Calculator that takes no parameters and creates
 	 * a new calculator object that contains a variable 'total' which is initalized to
@@ -56,6 +61,7 @@ public class Calculator {
 	 */
 	public void add (int value) {
 		total = total + value;
+		history = history + " + " + value;
 	}
 	
 	/**
@@ -65,6 +71,7 @@ public class Calculator {
 	 */
 	public void subtract (int value) {
 		total = total - value;
+		history = history + " - " + value;
 	}
 	
 	/**
@@ -74,6 +81,7 @@ public class Calculator {
 	 */
 	public void multiply (int value) {
 		total = total * value;
+		history = history + " * " + value;
 	}
 	
 	/**
@@ -90,14 +98,16 @@ public class Calculator {
 		{
 			total = total/value;
 		}
+		history = history + " / " + value;
 	}
 	
 	/**
 	 * The getHistory function will print all previous functions executed
 	 * with spaces in between each character.
 	 * @param No parameters used for this function.
+	 * returns a string the represents the arithmetic done.
 	 */
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
